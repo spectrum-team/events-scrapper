cda61a1b4da34e3cfd257a41e15fed5d8b215fab
Gustavo Moya <gmoyatorres@gmail.com>
2018-05-13 18:04:23 -0400

    Adding dependencies

---
+++ vendor/github.com/goodsign/monday/format_common.go
@@ -0,0 +1,174 @@
+package monday
+
+import "strings"
+
+func findInString(where string, what string, foundIndex *int, trimRight *int) (found bool) {
+	ind := strings.Index(strings.ToLower(where), strings.ToLower(what))
+	if ind != -1 {
+		*foundIndex = ind
+		*trimRight = len(where) - ind - len(what)
+		return true
+	}
+
+	return false
+}
+
+// commonFormatFunc is used for languages which don't have changed forms of month names dependent
+// on their position (after day or standalone)
+func commonFormatFunc(value, format string,
+	knownDaysShort, knownDaysLong, knownMonthsShort, knownMonthsLong, knownPeriods map[string]string) (res string) {
+	l := stringToLayoutItems(value)
+	f := stringToLayoutItems(format)
+	if len(l) != len(f) {
+		return value // layouts does not matches
+	}
+	for i, v := range l {
+
+		var knw map[string]string
+
+		// number of symbols before replaced term
+		foundIndex := 0
+		trimRight := 0
+		lowerCase := false
+		switch {
+		case findInString(f[i].item, "Monday", &foundIndex, &trimRight):
+			knw = knownDaysLong
+		case findInString(f[i].item, "Mon", &foundIndex, &trimRight):
+			knw = knownDaysShort
+		case findInString(f[i].item, "January", &foundIndex, &trimRight):
+			knw = knownMonthsLong
+		case findInString(f[i].item, "Jan", &foundIndex, &trimRight):
+			knw = knownMonthsShort
+		case findInString(f[i].item, "PM", &foundIndex, &trimRight):
+			knw = knownPeriods
+		case findInString(f[i].item, "pm", &foundIndex, &trimRight):
+			lowerCase = true
+			knw = knownPeriods
+		}
+
+		knw = mapToLowerCase(knw)
+
+		if knw != nil {
+			trimmedItem := strings.ToLower(v.item[foundIndex : len(v.item)-trimRight])
+
+			tr, ok := knw[trimmedItem]
+			if lowerCase == true {
+				tr = strings.ToLower(tr)
+			}
+
+			if ok {
+				res = res + v.item[:foundIndex] + tr + v.item[len(v.item)-trimRight:]
+			} else {
+				res = res + v.item
+			}
+		} else {
+			res = res + v.item
+		}
+	}
+	return res
+}
+
+func hasDigitBefore(l []dateStringLayoutItem, position int) bool {
+	if position >= 2 {
+		return l[position-2].isDigit && len(l[position-2].item) <= 2
+	}
+	return false
+}
+
+// commonGenitiveFormatFunc is used for languages with genitive forms of names, like Russian.
+func commonGenitiveFormatFunc(value, format string,
+	knownDaysShort, knownDaysLong, knownMonthsShort, knownMonthsLong,
+	knownMonthsGenShort, knownMonthsGenLong, knownPeriods map[string]string) (res string) {
+
+	l := stringToLayoutItems(value)
+	f := stringToLayoutItems(format)
+
+	if len(l) != len(f) {
+		return value // layouts does not matches
+	}
+
+	for i, v := range l {
+		lowerCase := false
+		var knw map[string]string
+		switch f[i].item {
+		case "Mon":
+			knw = knownDaysShort
+		case "Monday":
+			knw = knownDaysLong
+		case "Jan":
+			if hasDigitBefore(l, i) {
+				knw = knownMonthsGenShort
+			} else {
+				knw = knownMonthsShort
+			}
+		case "January":
+			if hasDigitBefore(l, i) {
+				knw = knownMonthsGenLong
+			} else {
+				knw = knownMonthsLong
+			}
+		case "PM":
+			knw = knownPeriods
+		case "pm":
+			lowerCase = true
+			knw = knownPeriods
+		}
+
+		knw = mapToLowerCase(knw)
+
+		if knw != nil {
+			tr, ok := knw[strings.ToLower(v.item)]
+			if !ok {
+				res = res + v.item
+				continue
+			}
+			if lowerCase == true {
+				tr = strings.ToLower(tr)
+			}
+			res = res + tr
+		} else {
+			res = res + v.item
+		}
+	}
+	return res
+}
+
+func createCommonFormatFunc(locale Locale) internalFormatFunc {
+	return func(value, layout string) (res string) {
+		return commonFormatFunc(value, layout,
+			knownDaysShort[locale], knownDaysLong[locale], knownMonthsShort[locale], knownMonthsLong[locale], knownPeriods[locale])
+	}
+}
+
+func createCommonFormatFuncWithGenitive(locale Locale) internalFormatFunc {
+	return func(value, layout string) (res string) {
+		return commonGenitiveFormatFunc(value, layout,
+			knownDaysShort[locale], knownDaysLong[locale], knownMonthsShort[locale], knownMonthsLong[locale],
+			knownMonthsGenitiveShort[locale], knownMonthsGenitiveLong[locale], knownPeriods[locale])
+	}
+}
+
+func createCommonParseFunc(locale Locale) internalParseFunc {
+	return func(layout, value string) string {
+		return commonFormatFunc(value, layout,
+			knownDaysShortReverse[locale], knownDaysLongReverse[locale],
+			knownMonthsShortReverse[locale], knownMonthsLongReverse[locale], knownPeriodsReverse[locale])
+	}
+}
+
+func createCommonParsetFuncWithGenitive(locale Locale) internalParseFunc {
+	return func(layout, value string) (res string) {
+		return commonGenitiveFormatFunc(value, layout,
+			knownDaysShortReverse[locale], knownDaysLongReverse[locale],
+			knownMonthsShortReverse[locale], knownMonthsLongReverse[locale],
+			knownMonthsGenitiveShortReverse[locale], knownMonthsGenitiveLongReverse[locale], knownPeriodsReverse[locale])
+	}
+}
+
+func mapToLowerCase(source map[string]string) map[string]string {
+	result := make(map[string]string)
+	for k, v := range source {
+		result[strings.ToLower(k)] = v
+	}
+	return result
+}
